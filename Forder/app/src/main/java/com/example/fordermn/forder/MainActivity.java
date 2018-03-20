cc29782877f8e1c6015e79370b20eb16c854fd0b
AvoidBFight <simonningsl@gmail.com>
2018-03-19 19:17:28 -0700

    Android

---
+++ Forder/app/src/main/java/com/example/fordermn/forder/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.fordermn.forder;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp, btnSignIn;
+    TextView txtSlogen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        txtSlogen = (TextView) findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/SSP-Regular.ttf");
+        txtSlogen.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
